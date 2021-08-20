5ed982fa4b7d1fa9ad36dbf8aa6fd94960b211ec
sohyunHwang <tinkle9696@gmail.com>
2021-08-21 00:00:17 +0900

    3강 따라하기

--- SngClient/Assets/Scripts/GameClient.cs
+++ SngClient/Assets/Scripts/GameClient.cs
@@ -21,7 +21,22 @@ public class GameClient : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_netClient.AttachProxy(m_C2SProxy);
+        m_netClient.AttachStub(m_S2CStub);
+
+        m_S2CStub.ReplyLogon = (Nettention.Proud.HostID remote, Nettention.Proud.RmiContext rmiContext, int result, String comment){
+            if( result == 0)
+            {
+                m_state = State.InVille;
+            }
+            else
+            {
+                m_state = State.Failed;
+                //m_failMessage
+            }
+        }
+
+
     }
 
     // Update is called once per frame
@@ -54,8 +69,8 @@ public class GameClient : MonoBehaviour
         }
     }
 
-   // SocialC2S.Proxy m_C2SProxy = new SocialC2S.Proxy();
-    //SocialS2S.Stub m_S2CStub = new SocialS2C.Stub();
+    SocialC2S.Proxy m_C2SProxy = new SocialC2S.Proxy();
+    SocialS2C.Stub m_S2CStub = new SocialS2C.Stub();
 
     private void IssueConnect()
     {
@@ -65,8 +80,12 @@ public class GameClient : MonoBehaviour
             {
                 m_state = State.LoggingOn;
                 m_loginButtonText = "Logging on...";
-               // m_loginButtonText = "Connected!";
-               //m_C2SProxy.RequestLogon(HostID.HostID_Server, RmiContext.ReliableSend, m_villeName, m_requestNEwVille)
+               
+                
+                
+                // m_loginButtonText = "Connected!";
+               // send logon message to server 
+                m_C2SProxy.RequestLogon(HostID.HostID_Server, RmiContext.ReliableSend, m_villeName, m_requestNEwVille)
             }
             else
             {
